6ca1e5091c7becf4b57e2ccfaac5bcfda674ce5b
Daniel <daniel.ginn@live.com.au>
2023-02-15 18:56:51 -0600

    Added metabolism function

--- life.py
+++ life.py
@@ -4,33 +4,38 @@ class Life:
 
     def __init__(self, genes: dict, birth_time: int):
         self.genes = genes
-        self.health = 100  # percentage
-        self.hunger = 0  # percentage
+        self.health = 100.0  # percentage
+        self.hunger = 0.0  # percentage
         self.grow_rate = genes['initial_grow_rate']  # how much does organism grow each clock cycle
         self.metabolic_rate = genes['initial_metabolic_rate'] + self.grow_rate  # rate at which hunger is consumed
         self.size = genes['initial_size']  # diameter of organism
         self.birth_time = birth_time  # clock date organism was created
 
     def time_update(self) -> None:
+        self.metabolize()
         self.grow()
         self.heal()
         self.starve()
 
     def grow(self) -> None:
-        if (self.health < 100) and (self.hunger < 100):
+        if (self.health < 100.0) and (self.hunger < 100.0):
             self.size += self.grow_rate/2
-        elif self.hunger < 100:
+        elif self.hunger < 100.0:
             self.size += self.grow_rate
         else:
             pass
 
     def heal(self):
-        if (self.health < 100) and (self.hunger < 100):
+        if (self.health < 100.0) and (self.hunger < 100.0):
             self.health += 1
 
     def starve(self):
-        if self.hunger == 100:
+        if self.hunger > 99.5:
             self.health -= 1
+
+    def metabolize(self):
+        if self.hunger < 99.5:
+            self.hunger += self.metabolic_rate
 
 
     # it can:
--- tests/test_life.py
+++ tests/test_life.py
@@ -49,3 +49,15 @@ class TestLife(TestCase):
         animal.time_update()
         self.assertEqual(animal.health, 49)
         self.assertAlmostEqual(animal.size, 2.0, 1)
+
+    def test_living_should_cost_hunger(self):
+        genes = {'initial_size': 2.0, 'initial_metabolic_rate': 0.5, 'initial_grow_rate': 0.0}
+        animal = Life(genes=genes, birth_time=0)
+        animal.time_update()
+        self.assertAlmostEqual(animal.hunger, 0.5, 1)
+
+    def test_living_and_growing_should_cost_hunger(self):
+        genes = {'initial_size': 2.0, 'initial_metabolic_rate': 0.5, 'initial_grow_rate': 0.5}
+        animal = Life(genes=genes, birth_time=0)
+        animal.time_update()
+        self.assertAlmostEqual(animal.hunger, 1.0, 1)
