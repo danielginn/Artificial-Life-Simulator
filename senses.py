91479a3c25cc5168c99c93b5a050dced48bbf5fd
Daniel <daniel.ginn@live.com.au>
2023-02-20 20:21:39 -0600

    Created a find_food function for vision class

--- senses.py
+++ senses.py
@@ -1,9 +1,33 @@
+from movement import Movement
+from math import atan2, degrees, sqrt
+from helper_functions import degrees_check
+import sys
 
-
-class Vision:
-    def __init__(self, max_range: float, max_fov: float):
+class Vision(Movement):
+    def __init__(self, max_range: float, max_fov: float, x:float, y:float, heading: float):
         self.max_range = max_range
         self.max_fov = max_fov
+        self.x = x
+        self.y = y
+        self.heading = heading
+        self.foodVisible = False
+
+    def find_nearest_food(self, food_list: list[list[float, float]]) -> float:
+        nearest_food_distance = sys.float_info.max
+        nearest_food_heading = self.heading
+        self.foodVisible = False
+        for food_item in food_list:
+            y_diff = food_item[1] - self.y
+            x_diff = food_item[0] - self.x
+            angle_food = degrees(atan2(y_diff,x_diff))
+            angle_diff = degrees_check(angle_food - self.heading)
+            dist = sqrt(x_diff*x_diff + y_diff*y_diff)
+            if (abs(angle_diff) < self.max_fov) and (dist < self.max_range) and (dist < nearest_food_distance):
+                nearest_food_distance = dist
+                nearest_food_heading = angle_diff
+                self.foodVisible = True
+        return nearest_food_heading
+
 
 
     # Vision functionalities
--- tests/test_senses.py
+++ tests/test_senses.py
@@ -4,6 +4,34 @@ from senses import Vision
 
 class TestVision(TestCase):
     def test_Vision_initialization(self):
-        vision = Vision(max_range=10, max_fov=20)
+        vision = Vision(max_range=10, max_fov=20, x=0, y=0, heading=0)
         self.assertAlmostEqual(vision.max_range, 10.0, 1)
         self.assertAlmostEqual(vision.max_fov, 20.0, 1)
+
+    def test_should_find_nearest_of_2_food_pellets(self):
+        food_list = [[2, 2], [3, -3]]
+        vision = Vision(max_range=10, max_fov=50, x=0, y=0, heading=0)
+        self.assertAlmostEqual(vision.find_nearest_food(food_list=food_list), 45.0, 1)
+        self.assertEqual(vision.foodVisible, True)
+        food_list = [[3, 3], [2, -2]]
+        self.assertAlmostEqual(vision.find_nearest_food(food_list=food_list), -45.0, 1)
+        self.assertEqual(vision.foodVisible, True)
+
+    def test_should_not_find_food_out_of_sight_range(self):
+        food_list = [[20, 20], [30, -30]]
+        vision = Vision(max_range=10, max_fov=50, x=0, y=0, heading=0)
+        food_angle = vision.find_nearest_food(food_list=food_list)
+        self.assertEqual(vision.foodVisible, False)
+
+    def test_should_not_find_food_out_of_sight_fov(self):
+        food_list = [[-2, 2], [-3, -3]]
+        vision = Vision(max_range=10, max_fov=50, x=0, y=0, heading=0)
+        food_angle = vision.find_nearest_food(food_list=food_list)
+        self.assertEqual(vision.foodVisible, False)
+
+    def test_should_find_food_when_heading_neg_180(self):
+        food_list = [[-2, 2], [-3, -3]]
+        vision = Vision(max_range=10, max_fov=50, x=0, y=0, heading=-180)
+        self.assertAlmostEqual(vision.find_nearest_food(food_list=food_list), -45.0, 1)
+        self.assertEqual(vision.foodVisible, True)
+
