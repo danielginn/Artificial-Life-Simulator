3f85389967bcc5e332fe9c475432039f20f45126
Daniel <daniel.ginn@live.com.au>
2023-10-28 23:09:33 +1100

    added multiple simucells

--- pygameEngine.py
+++ pygameEngine.py
@@ -2,6 +2,7 @@ import pygame
 from simucell import SimuCell
 import numpy as np
 from world import World
+import random
 
 pygame.init()
 world = World()
@@ -9,19 +10,24 @@ SCREEN_WIDTH = world.width
 SCREEN_HEIGHT = world.height
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-FRAME_RATE = 60
+FRAME_RATE = 30
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-simucell = SimuCell(world, np.array([25.0, 25.0]), -15.0, size=50)
-simucell.speed = 10
+num_simucells = 50
+simucells = []
+for i in range(num_simucells):
+    location = np.array([random.uniform(25.0,world.width), random.uniform(25.0,world.height)])
+    simucells.append(SimuCell(world, location, random.uniform(-180,180), size=10))
+    simucells[i].speed = random.uniform(5.0,15.0)
 
 run = True
 while run:
     screen.fill(BLACK)
-    simucell.update_location()
-    pygame.draw.rect(screen, RED, simucell.get_sprite())
+    for simucell in simucells:
+        simucell.update_location()
+        pygame.draw.rect(screen, RED, simucell.get_sprite())
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
