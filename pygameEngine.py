1245aeb970c88ee6ca2319bcdcdd6ba8e5ec0a39
Daniel <daniel.ginn@live.com.au>
2023-10-29 14:11:02 +1100

    Using bibite sprites

--- pygameEngine.py
+++ pygameEngine.py
@@ -5,6 +5,7 @@ from world import World
 import random
 import yaml
 
+
 with open('engine.yml', 'r') as file:
     config = yaml.safe_load(file)
 world = World()
@@ -13,30 +14,31 @@ pygame.init()
 SCREEN_WIDTH = world.width
 SCREEN_HEIGHT = world.height
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+DARK_GREEN = (0, 163, 0)
 RED = (255, 0, 0)
+LIGHT_GRAY = (211, 211, 211)
 frame_rate = int(config['frame_rate'])
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 num_simucells = int(config['num_simucells'])
-simucells = []
+simucells = pygame.sprite.Group()
 for i in range(num_simucells):
-    size = random.randint(10, 30)
+    size = 45
     location = np.array([random.uniform(size//2,world.width-size//2), random.uniform(size//2,world.height-size//2)])
-    simucells.append(SimuCell(world, location, random.uniform(-180,180), size=size))
-    simucells[i].speed = random.uniform(5.0,15.0)
+    simucells.add(SimuCell(world, location, random.uniform(-180,180), size, speed=random.uniform(5.0,10.0)))
 
 run = True
 while run:
-    screen.fill(BLACK)
-    for simucell in simucells:
-        simucell.update_location()
-        pygame.draw.rect(screen, RED, simucell.get_sprite())
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.fill(LIGHT_GRAY)
+    simucells.draw(screen)
+    simucells.update()
 
     pygame.display.update()
     clock.tick(frame_rate)
--- simucell.py
+++ simucell.py
@@ -4,27 +4,36 @@ import pygame
 from world import World
 
 
-class SimuCell:
+class SimuCell(pygame.sprite.Sprite):
+    image: pygame.image
+    original_image: pygame.image
+    rect: pygame.Rect
     world: World # Reference to the world the SimuCell lives in
     location: np.ndarray # x right, y down
     heading: float # degrees in the clockwise direction from the x-axis
     speed: float # pixels per frame
     size: int
 
-    def __init__(self, world: World, initLocation: np.ndarray, initHeading: float, size: int = 1):
+    def __init__(self, world: World, initLocation: np.ndarray, initHeading: float, size: int = 1, speed: float = 0.0):
+        super().__init__()
         self.world = world
         self.location = initLocation.copy()
         self.heading = initHeading
-        self.speed = 0
+        self.original_image = pygame.image.load("bibite.png")
+        self.original_image = pygame.transform.scale(self.original_image, (39, 45))
+        self.image = self.original_image.copy()
+        self.image = pygame.transform.rotate(self.original_image, -self.heading)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.location[0],self.location[1])
         self.size = size
+        self.speed = speed
 
-    def update_location(self) -> None:
+    def update(self) -> None:
         velocity = self.speed * np.array([cos(radians(self.heading)), sin(radians(self.heading))])
         self.location += velocity
         boundary_check_code = self.world.boundary_check(self.location, self.size)
         if boundary_check_code > 0:
             self.location -= velocity
-            old_velocity = velocity.copy()
             match boundary_check_code:
                 case 1:
                     velocity[0] *= -1
@@ -33,11 +42,9 @@ class SimuCell:
                 case 3:
                     velocity *= -1
             self.location += velocity
-            self.update_heading(velocity)
-
-    def update_heading(self, velocity: np.ndarray) -> None:
         self.heading = degrees(atan2(velocity[1], velocity[0]))
+        self.rect.center = (self.location[0], self.location[1])
+        self.image = pygame.transform.rotate(self.original_image, -self.heading)
 
-
-    def get_sprite(self) -> pygame.Rect:
-        return pygame.Rect(int(self.location[0])-self.size//2, int(self.location[1])-self.size//2, self.size, self.size)
+    # def get_sprite(self) -> pygame.Rect:
+    #     return pygame.Rect(int(self.location[0])-self.size//2, int(self.location[1])-self.size//2, self.size, self.size)
